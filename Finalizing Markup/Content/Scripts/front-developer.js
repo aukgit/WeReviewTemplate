e27bff8bd30c9433534810d9e9a99a50cc511bcc
Alim Ul Karim <devorg.bd@gmail.com>
2015-02-26 18:22:11 +0600

    appbox centered

--- Finalizing Markup/Content/Scripts/front-developer.js
+++ Finalizing Markup/Content/Scripts/front-developer.js
@@ -53,8 +53,9 @@ $(function () {
         ],
         items: 6, //10 items above 1000px browser width
         itemsDesktop: [1000, 5], //5 items between 1000px and 901px
-        itemsDesktopSmall: [900, 3], // betweem 900px and 601px
-        itemsTablet: [600, 2] //2 items between 600 and 0
+        itemsDesktopSmall: [900, 4], // betweem 900px and 601px
+        itemsTablet: [600, 3], //2 items between 600 and 0
+        itemsMobile:[450, 2]
 
     });
 
