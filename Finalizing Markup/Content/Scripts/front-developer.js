377b02d0cf71ec68d1fb4fb603c72f61fd21c0fa
Alim Ul Karim <devorg.bd@gmail.com>
2015-02-28 03:24:19 +0600

    app-details-changes-2

--- Finalizing Markup/Content/Scripts/front-developer.js
+++ Finalizing Markup/Content/Scripts/front-developer.js
@@ -66,4 +66,20 @@ $(function () {
         showClear: false,
         showCaption: false
     });
+
+    $("#owl-single").owlCarousel({
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true,
+        items: 1,
+        itemsDesktop: false,
+        itemsDesktopSmall: false,
+        itemsTablet: false,
+        itemsMobile: false,
+        stopOnHover: true,
+        navigation: true, // Show next and prev buttons
+        pagination: false,
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+    });
+
 });
